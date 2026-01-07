633648cde0e8a615f6af82dc412a1c1a11c40505
Volodymyr But <git@hiegz.com>
2026-01-07 08:16:17 +0100

    OOPV2526: Übungsblatt 12

---
+++ OOPV2526/ueb12/Maven-Demo/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if (args.length < 3) {
+			throw new IllegalArgumentException();
+		}
+		
+		System.out.println(StringUtils.replaceIgnoreCase(args[0], args[1], args[2]));
+	}
+
+}
